686f4ef336f18c32653d4481358ff92839d35073
Sanjay Y Patel <sanjay.y.patel@gmail.com>
2015-06-02 11:29:48 -0700

    Assignment 23, Create spec for a second csv file.

--- specs/address_book_specs.rb
+++ specs/address_book_specs.rb
@@ -69,5 +69,27 @@ RSpec.describe AddressBook do
       check_entry(entry_five, "Sussie", "555-555-5555", "sussie@blocmail.com")
     end
   end
+  describe ".import_from_csv" do
+    it "imports the correct number of entries from another file" do
+      book.import_from_csv("entries_2.csv")
+      book_size = book.entries.size
+      expect(book_size).to eql 3
+    end
+    it "checks the details of the first entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_one = book.entries[0]
+      check_entry(entry_one, "Beaker", "123-456-7890", "beaker@muppets.net")
+    end
+    it "checks the details of the second entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_two = book.entries[1]
+      check_entry(entry_two, "Animal", "123-456-7890", "animal@muppets.net")
+    end
+    it "checks the details of the third entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_three = book.entries[2]
+      check_entry(entry_three, "Rizzo", "123-456-7890", "rizzo@muppets.net")
+    end
+  end
 
 end
