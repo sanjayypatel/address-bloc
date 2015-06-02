5325aefd99fc763734f9b4f15d1a11432258ec73
Sanjay Y Patel <sanjay.y.patel@gmail.com>
2015-06-02 13:28:48 -0700

    Checkpoint 24 Search, Complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -19,6 +19,30 @@ class AddressBook
     @entries.insert(index, Entry.new(name, phone_number, email))
   end
 
+  def binary_search(name)
+    # set upper and lower to first/last indices
+    lower = 0
+    upper = entries.length - 1
+
+    # repeat comparisons until upper and lower cross
+    while lower <= upper
+      # compare the midpoint of the collection to search term
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+      if name == mid_name
+        # if it's found, return it
+        return entries[mid]
+      elsif name < mid_name
+        # if the name comes before the mid_name alphabetically, change the upper limit
+        upper = mid - 1
+      elsif name > mid_name
+        # if the name comes after the mid_name alphabetically, change the lower limit
+        lower = mid + 1
+      end
+    end
+    return nil
+  end
+
   def import_from_csv(file_name)
     csv_text = File.read(file_name)
     csv = CSV.parse(csv_text, headers: true)
