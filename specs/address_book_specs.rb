b81553f9616d3381c26bdc5c08991dc711480a17
Sanjay Y Patel <sanjay.y.patel@gmail.com>
2015-06-02 11:15:38 -0700

    Fixed location of entries.csv

--- specs/address_book_specs.rb
+++ specs/address_book_specs.rb
@@ -39,33 +39,32 @@ RSpec.describe AddressBook do
 
   describe ".import_from_csv" do
     it "imports the correct number of entries" do
-      # Was getting 'No Such File' Error with 'entries.csv' path
-      book.import_from_csv("models/entries.csv")
+      book.import_from_csv("entries.csv")
       book_size = book.entries.size
       expect(book_size).to eql 5
     end
     it "checks the details of the first entry" do
-      book.import_from_csv("models/entries.csv")
+      book.import_from_csv("entries.csv")
       entry_one = book.entries[0]
       check_entry(entry_one, "Bill", "555-555-5555", "bill@blocmail.com")
     end
     it "checks the details of the second entry" do
-      book.import_from_csv("models/entries.csv")
+      book.import_from_csv("entries.csv")
       entry_two = book.entries[1]
       check_entry(entry_two, "Bob", "555-555-5555", "bob@blocmail.com")
     end
     it "checks the details of the third entry" do
-      book.import_from_csv("models/entries.csv")
+      book.import_from_csv("entries.csv")
       entry_three = book.entries[2]
       check_entry(entry_three, "Joe", "555-555-5555", "joe@blocmail.com")
     end
     it "checks the details of the fourth entry" do
-      book.import_from_csv("models/entries.csv")
+      book.import_from_csv("entries.csv")
       entry_four = book.entries[3]
       check_entry(entry_four, "Sally", "555-555-5555", "sally@blocmail.com")
     end
     it "checks the details of the fifth entry" do
-      book.import_from_csv("models/entries.csv")
+      book.import_from_csv("entries.csv")
       entry_five = book.entries[4]
       check_entry(entry_five, "Sussie", "555-555-5555", "sussie@blocmail.com")
     end
